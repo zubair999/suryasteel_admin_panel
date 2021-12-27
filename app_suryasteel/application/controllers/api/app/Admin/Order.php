7957738ff1c47edf799cfd0da4ef0bd367afdf68
zubair <imzubairkhan786@gmail.com>
2021-12-27 21:54:20 +0530

    some change

--- app_suryasteel/application/controllers/api/app/Admin/Order.php
+++ app_suryasteel/application/controllers/api/app/Admin/Order.php
@@ -21,7 +21,7 @@ class Order extends REST_Controller
             if($isOrderAdded['response']){
                 $this->order_m->addOrderItem($isOrderAdded['last_order_id']);
                 $user = $this->auth_m->getUserById($data['userId']);
-                $this->onesignal_m->send_push_notification($user->customer_company);
+                // $this->onesignal_m->send_push_notification($user->customer_company);
                 $response = ['status' => 200, 'message' =>'success', 'description' =>"Order created successfully."];
             }
             else{
