de057e6b64dd2ba2469cf7e7d1f49a1d62afa61f
zubair <imzubairkhan786@gmail.com>
2022-03-01 13:19:30 +0530

    some changes

--- app_suryasteel/application/controllers/api/app/Admin/Acidtreatment.php
+++ app_suryasteel/application/controllers/api/app/Admin/Acidtreatment.php
@@ -55,8 +55,6 @@ class Acidtreatment extends REST_Controller
 			} else {
                 $isAdded = $this->acidtreatment_m->addAcidTreatmentHistory($this->input->post('completedBy'));
                 if($isAdded['status'] == 'success'){
-                    $lastAcidTreatmentHistoryId = $this->db->insert_id();
-                    $this->draw_m->addDrawProcess($this->input->post('roundLengthCompleted') , $lastAcidTreatmentHistoryId);
                     $response = ['status' => 200, 'message' => 'success', 'description' => 'Acid treatment completed for the batch successfully.'];
                 }
                 else{
